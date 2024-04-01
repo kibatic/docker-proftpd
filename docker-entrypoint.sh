c1c4950bd2ee6e67d3078dd02fa5143f5caa9030
AvaLanCS <avalancsreg@gmail.com>
2024-04-01 16:30:41 +0200

    Print error and stop container if creating users was unsuccessful

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Parse users from FTP_LIST, create them and chown their home directories
 if [ -n "$FTP_LIST" ]; then
+	echo "Parsing user list and creating home folders..."
 	IFS=';' read -r -a parsed_ftp_list <<< "$FTP_LIST" ; unset IFS
 	for ftp_account in ${parsed_ftp_list[@]}
 	do
@@ -8,9 +10,9 @@ if [ -n "$FTP_LIST" ]; then
 		ftp_login=${tab[0]}
 		ftp_pass=${tab[1]}
 		CRYPTED_PASSWORD=$(perl -e 'print crypt($ARGV[0], "password")' $ftp_pass)
-		useradd --shell /bin/sh ${USERADD_OPTIONS} -d /home/$ftp_login --password $CRYPTED_PASSWORD $ftp_login
-    echo "ftp_login: $ftp_login"
-    chown -R $ftp_login:$ftp_login /home/$ftp_login
+		useradd --shell /bin/sh ${USERADD_OPTIONS} -d /home/$ftp_login --password $CRYPTED_PASSWORD $ftp_login || { echo "Creating user $ftp_login failed! Check previous log message." ; exit 1; }
+    	echo "ftp_login: $ftp_login"
+    	chown -R $ftp_login:$ftp_login /home/$ftp_login || { echo "Failed to chown home folder for $ftp_login ! Check previous log message." ; exit 1; }
 	done
 fi
 
